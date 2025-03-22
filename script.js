2ba35af2f5e1f41112e7535b95480b231018742f
fireboy-glitch <rizanmk27@gmail.com>
2025-03-22 19:17:29 +0545

    made a major change

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loader = document.getElementById("loading-screen");
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 1500);
+});
